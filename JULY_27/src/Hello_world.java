11e0b470eab1a92f00e8526b3d89a1c34721270a
sagar somaiah <somaiah1988@gmail.com>
2021-07-27 07:47:43 +0530

    basic java intro

---
+++ JULY_27/src/Hello_world.java
@@ -0,0 +1,24 @@
+
+public class Hello_world
+{
+
+	
+	public static void main(String[] args)
+	{
+		
+		System.out.println("hello world");
+		
+		System.out.print("Welcome to janbask");
+		
+		System.out.print("Selenium is a automation tool");
+		System.out.println();
+		
+		System.out.print("Java is a programming language");
+		
+		System.out.println("Testng is a testing framework");
+		
+		
+
+	
+	}
+}
