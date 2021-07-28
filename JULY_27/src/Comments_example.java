b441d72e2e7212a07b3ad686c16e82b87641e175
sagar somaiah <somaiah1988@gmail.com>
2021-07-28 06:28:21 +0530

    day2

--- JULY_27/src/Comments_example.java
+++ JULY_27/src/Comments_example.java
@@ -6,24 +6,26 @@ public class Comments_example {
 		
         System.out.println("hello world");
 		
-		/*
-		 * System.out.print("Welcome to janbask");
-		 * 
-		 * System.out.println("Selenium is a automation tool");
-		 * 
-		 * 
-		 * System.out.println("Java is a programming language");
-		 * 
-		 * System.out.println("Testng is a testing framework");
-		 * System.out.println("hello world");
-		 * 
-		 * System.out.print("Welcome to janbask");
-		 */
+		
+		  System.out.print("Welcome to janbask");
+		  
+		  System.out.println("Selenium is a automation tool");
+		  
+		  
+			/*
+			 * System.out.println("Java is a programming language");
+			 * 
+			 * System.out.println("Testng is a testing framework");
+			 * System.out.println("hello world");
+			 * 
+			 * System.out.print("Welcome to janbask");
+			 */
+		 
 		
 		System.out.println("Selenium is a automation tool");
 		
 		
-		System.out.println("Java is a programming language");
+		//System.out.println("Java is a programming language");
 		
 		System.out.println("Testng is a testing framework");
 		
