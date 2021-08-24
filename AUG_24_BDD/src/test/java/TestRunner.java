d6f09dfdd363fd77a472c7dfa8439a5e8367a908
sagar somaiah <somaiah1988@gmail.com>
2021-08-24 08:01:40 +0530

    aug 24

--- AUG_24_BDD/src/test/java/TestRunner.java
+++ AUG_24_BDD/src/test/java/TestRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 
 @CucumberOptions(plugin = { "html:target/cucumber-html-report",
        },
-features={"D:\\JANBASK\\JULY_WEEKDAY_BATCH_2\\AUG_24_BDD\\features" },
+features={"D:\\JANBASK\\JULY_WEEKDAY_BATCH_2\\AUG_24_BDD\\features\\login.feature" },
 glue={"StepDefinition"})
 public class TestRunner {
 	
--- AUG_24_BDD/target/cucumber-html-report/report.js
+++ AUG_24_BDD/target/cucumber-html-report/report.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("login.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/JANBASK/JULY_WEEKDAY_BATCH_2/AUG_24_BDD/features/login.feature");
 formatter.feature({
   "line": 1,
   "name": "Login",
@@ -21,7 +21,7 @@ formatter.step({
 });
 formatter.step({
   "line": 4,
-  "name": "there user logs in through Login Window by using Username as \"USER\" and Password as \"PASSWORD\"",
+  "name": "there user logs in through Login Window by using Username as \"standard_user\" and Password as \"secret_sauce\"",
   "keyword": "And "
 });
 formatter.step({
@@ -29,16 +29,35 @@ formatter.step({
   "name": "login must be successful.",
   "keyword": "Then "
 });
-formatter.match({});
-formatter.result({
-  "status": "undefined"
+formatter.match({
+  "location": "LoginStepDefinition.launch_the_browser()"
 });
-formatter.match({});
 formatter.result({
-  "status": "undefined"
+  "duration": 3006443800,
+  "status": "passed"
 });
-formatter.match({});
+formatter.match({
+  "arguments": [
+    {
+      "val": "standard_user",
+      "offset": 62
+    },
+    {
+      "val": "secret_sauce",
+      "offset": 94
+    }
+  ],
+  "location": "LoginStepDefinition.there_user_logs_in_through_Login_Window_by_using_Username_as_and_Password_as(String,String)"
+});
 formatter.result({
-  "status": "undefined"
+  "duration": 551000500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "LoginStepDefinition.login_must_be_successful()"
+});
+formatter.result({
+  "duration": 204133000,
+  "status": "passed"
 });
 });
